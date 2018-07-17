6166891e2c223f1b0c5edd0495a77c16131baf6f
优雅的攻城狮 <just.public@icloud.com>
2018-07-17 18:08:09 +0800

    添加示例路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,14 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
+//
+import NotFuoud from '@/pages/common/404';
+import NoAccess from '@/pages/common/403';
+import ServiceError from '@/pages/common/500';
+
+//
+import Login from '@/pages/common/Login';
+import Home from '@/pages/home/home';
 
 Vue.use(Router);
 
@@ -10,6 +18,26 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    }, {
+      path: '/403',
+      name: 'NoAccess',
+      component: NoAccess
+    }, {
+      path: '/404',
+      name: 'NotFuoud',
+      component: NotFuoud
+    }, {
+      path: '/500',
+      name: 'ServiceError',
+      component: ServiceError
+    }, {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    }, {
+      path: '/home',
+      name: 'Home',
+      component: Home
     }
   ]
 });
